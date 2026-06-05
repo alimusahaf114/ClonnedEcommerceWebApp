47bacefb6c39a260db058afc954d064be3c9da9d
Musahaf Ali <musahafali43@gmail.com>
2026-06-05 15:44:11 +0500

    created proper folder structure fo everything

---
+++ musahafCommerce/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./App.jsx"
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-blue-500 text-white'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
